20d0b47ee061fc3cc7e86a7eea42dd2621ad0f08
RikkaW <rikka@shizuku.moe>
2021-05-08 23:01:04 +0800

    Find com.android.build.gradle.BaseExtension

--- src/main/java/dev/rikka/tools/HiddenApiRefinePlugin.java
+++ src/main/java/dev/rikka/tools/HiddenApiRefinePlugin.java
@@ -1,11 +1,12 @@
 package dev.rikka.tools;
 
-import com.android.build.api.transform.Transform;
+import com.android.build.gradle.BaseExtension;
+
 import org.gradle.api.GradleException;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 
-import java.lang.reflect.Method;
+import java.util.List;
 import java.util.function.Function;
 
 @SuppressWarnings("unused")
@@ -13,15 +14,17 @@ public class HiddenApiRefinePlugin implements Plugin<Project> {
 
     @Override
     public void apply(Project target) {
-        if (target.getExtensions().findByName("android") == null)
-            throw new GradleException("Extension `android` not found");
+        BaseExtension androidExtension = target.getExtensions().findByType(BaseExtension.class);
+        if (androidExtension == null)
+            throw new GradleException("Android extension not found");
 
         final HiddenApiRefineExtension extension = target.getExtensions()
                 .create("hiddenApiRefine", HiddenApiRefineExtension.class);
 
         final boolean log = extension.getLog().get();
+        final List<String> prefixToRemove = extension.getPrefixToRemove().get();
 
-        final Function<String, String> rename = (String name) -> extension.getPrefixToRemove().get().stream()
+        final Function<String, String> rename = (String name) -> prefixToRemove.stream()
                 .reduce(name, (String n, String p) -> {
                     if (n.startsWith(p)) {
                         String newName = n.substring(p.length());
@@ -33,19 +36,6 @@ public class HiddenApiRefinePlugin implements Plugin<Project> {
                     return n;
                 });
 
-        Object androidExtension = target.getExtensions().getByName("android");
-
-        try {
-            Method registerTransform = androidExtension.getClass()
-                    .getMethod("registerTransform", Transform.class, Object[].class);
-
-            registerTransform.invoke(
-                    androidExtension,
-                    new HiddenApiRefineTransform(rename),
-                    new Object[0]
-            );
-        } catch (Exception e) {
-            throw new GradleException("Register HiddenApiRefineTransform failed", e);
-        }
+        androidExtension.registerTransform(new HiddenApiRefineTransform(rename));
     }
 }
