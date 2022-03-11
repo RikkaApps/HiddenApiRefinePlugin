085df5808445b9a8418ba4475224bdbc4cc210ea
kr328 <kr328app@outlook.com>
2022-03-11 19:55:18 +0800

    Use marker artifact

--- gradle-plugin/build.gradle.kts
+++ gradle-plugin/build.gradle.kts
@@ -3,35 +3,22 @@ plugins {
     `java-gradle-plugin`
 }
 
-val pluginId = "$group.$name"
-val pluginClass = "$group.RefinePlugin"
-
-repositories {
-    mavenCentral()
-    google()
-}
-
 dependencies {
     compileOnly(gradleApi())
 
     implementation(project(":annotation"))
     implementation(project(":annotation-processor"))
 
-    compileOnly("com.android.tools.build:gradle:7.0.1")
-    implementation("com.google.code.gson:gson:2.8.8")
-    implementation("org.javassist:javassist:3.28.0-GA")
-}
-
-java {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
+    compileOnly(deps.android.gradle)
+    implementation(deps.google.gson)
+    implementation(deps.javassist)
 }
 
 gradlePlugin {
     plugins {
         create("HiddenApiRefine") {
-            id = pluginId
-            implementationClass = pluginClass
+            id = project.group.toString()
+            implementationClass = "$id.RefinePlugin"
         }
     }
 }
--- runtime/build.gradle.kts
+++ runtime/build.gradle.kts
@@ -1,8 +1,3 @@
 plugins {
     java
 }
-
-java {
-    sourceCompatibility = JavaVersion.VERSION_1_8
-    targetCompatibility = JavaVersion.VERSION_1_8
-}
