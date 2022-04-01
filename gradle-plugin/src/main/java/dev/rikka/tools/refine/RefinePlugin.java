5d9843dc9d0783ffcda824d9988592a64f9849fc
Kr328 <kr328app@outlook.com>
2022-04-02 00:26:41 +0800

    Fix: fix incorrect afterEvaluate usage

--- gradle-plugin/src/main/java/dev/rikka/tools/refine/RefinePlugin.java
+++ gradle-plugin/src/main/java/dev/rikka/tools/refine/RefinePlugin.java
@@ -2,7 +2,6 @@ package dev.rikka.tools.refine;
 
 import com.android.build.gradle.BaseExtension;
 import com.android.build.gradle.LibraryExtension;
-import org.gradle.api.GradleException;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 
@@ -10,11 +9,9 @@ import org.gradle.api.Project;
 public class RefinePlugin implements Plugin<Project> {
     @Override
     public void apply(Project target) {
-        BaseExtension androidExtension = target.getExtensions().findByType(BaseExtension.class);
-        if (androidExtension == null)
-            throw new GradleException("Android extension not found");
-
-        boolean isLibrary = androidExtension instanceof LibraryExtension;
-        androidExtension.registerTransform(new RefineTransform(isLibrary));
+        target.getPlugins().withId("com.android.base", plugin -> {
+            final BaseExtension base = target.getExtensions().getByType(BaseExtension.class);
+            base.registerTransform(new RefineTransform(base instanceof LibraryExtension));
+        });
     }
 }
