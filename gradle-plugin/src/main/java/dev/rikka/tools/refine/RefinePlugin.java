396ccd6fde84a235ba7531445bd27298485bd395
Christopher-Marcel Esser <chris.esser.91@gmail.com>
2023-02-03 14:42:56 +0100

    Simplify component configuration
    Co-authored-by: RikkaW <rikka@shizuku.moe>

--- gradle-plugin/src/main/java/dev/rikka/tools/refine/RefinePlugin.java
+++ gradle-plugin/src/main/java/dev/rikka/tools/refine/RefinePlugin.java
@@ -1,12 +1,9 @@
 package dev.rikka.tools.refine;
 
 import com.android.build.api.instrumentation.InstrumentationScope;
-import com.android.build.api.variant.ApplicationAndroidComponentsExtension;
+import com.android.build.api.variant.AndroidComponentsExtension;
 import com.android.build.api.variant.Component;
 import com.android.build.api.variant.HasAndroidTest;
-import com.android.build.api.variant.LibraryAndroidComponentsExtension;
-import com.android.build.api.variant.Variant;
-
 import kotlin.Unit;
 import org.gradle.api.GradleException;
 import org.gradle.api.Plugin;
@@ -26,27 +23,14 @@ public class RefinePlugin implements Plugin<Project> {
             throw new GradleException("This plugin must be applied after `com.android.application` or `com.android.library`.");
         }
 
-        final ApplicationAndroidComponentsExtension applicationComponents =
-                target.getExtensions().findByType(ApplicationAndroidComponentsExtension.class);
-        if (applicationComponents != null) {
-            applicationComponents.onVariants(applicationComponents.selector().all(), variant -> {
-                configureVariant(variant);
-            });
-        }
-
-        final LibraryAndroidComponentsExtension libraryComponents =
-                target.getExtensions().findByType(LibraryAndroidComponentsExtension.class);
-        if (libraryComponents != null) {
-            libraryComponents.onVariants(libraryComponents.selector().all(), variant -> {
-                configureVariant(variant);
-            });
-        }
-    }
-
-    private <T extends Variant & HasAndroidTest> void configureVariant(@Nonnull final T variant) {
-        configureComponent(variant);
-        configureComponent(variant.getUnitTest());
-        configureComponent(variant.getAndroidTest());
+        final AndroidComponentsExtension<?, ?, ?> components = target.getExtensions().getByType(AndroidComponentsExtension.class);
+        components.onVariants(components.selector().all(), variant -> {
+            configureComponent(variant);
+            configureComponent(variant.getUnitTest());
+            if (variant instanceof HasAndroidTest) {
+                configureComponent(((HasAndroidTest) variant).getAndroidTest());
+            }
+        });
     }
 
     private void configureComponent(@Nullable final Component component) {
