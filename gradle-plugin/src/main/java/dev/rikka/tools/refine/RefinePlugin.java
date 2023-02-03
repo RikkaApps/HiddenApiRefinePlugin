aad32e5813eb768023fa3dff07a9e9d503b11351
Christopher-Marcel Esser <chris.esser.91@gmail.com>
2023-02-03 11:19:09 +0100

    Also transform unit and instrumentation tests

--- gradle-plugin/src/main/java/dev/rikka/tools/refine/RefinePlugin.java
+++ gradle-plugin/src/main/java/dev/rikka/tools/refine/RefinePlugin.java
@@ -1,13 +1,19 @@
 package dev.rikka.tools.refine;
 
 import com.android.build.api.instrumentation.InstrumentationScope;
-import com.android.build.api.variant.AndroidComponentsExtension;
+import com.android.build.api.variant.ApplicationAndroidComponentsExtension;
+import com.android.build.api.variant.Component;
+import com.android.build.api.variant.HasAndroidTest;
+import com.android.build.api.variant.LibraryAndroidComponentsExtension;
+import com.android.build.api.variant.Variant;
+
 import kotlin.Unit;
 import org.gradle.api.GradleException;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 
 /**
  * Gradle plugin that do the class rename works.
@@ -20,13 +26,36 @@ public class RefinePlugin implements Plugin<Project> {
             throw new GradleException("This plugin must be applied after `com.android.application` or `com.android.library`.");
         }
 
-        final AndroidComponentsExtension<?, ?, ?> components = target.getExtensions().getByType(AndroidComponentsExtension.class);
-        components.onVariants(components.selector().all(), variant -> {
-            variant.getInstrumentation().transformClassesWith(
+        final ApplicationAndroidComponentsExtension applicationComponents =
+                target.getExtensions().findByType(ApplicationAndroidComponentsExtension.class);
+        if (applicationComponents != null) {
+            applicationComponents.onVariants(applicationComponents.selector().all(), variant -> {
+                configureVariant(variant);
+            });
+        }
+
+        final LibraryAndroidComponentsExtension libraryComponents =
+                target.getExtensions().findByType(LibraryAndroidComponentsExtension.class);
+        if (libraryComponents != null) {
+            libraryComponents.onVariants(libraryComponents.selector().all(), variant -> {
+                configureVariant(variant);
+            });
+        }
+    }
+
+    private <T extends Variant & HasAndroidTest> void configureVariant(@Nonnull final T variant) {
+        configureComponent(variant);
+        configureComponent(variant.getUnitTest());
+        configureComponent(variant.getAndroidTest());
+    }
+
+    private void configureComponent(@Nullable final Component component) {
+        if (component != null) {
+            component.getInstrumentation().transformClassesWith(
                     RefineFactory.class,
                     InstrumentationScope.ALL,
                     (parameters) -> Unit.INSTANCE
             );
-        });
+        }
     }
 }
