ab421342411a604e480bf7ee25f632f3599cd3da
Christopher-Marcel Esser <chris.esser.91@gmail.com>
2023-02-03 16:13:32 +0100

    Also transform unit and instrumentation tests (#19)
    * Also transform unit and instrumentation tests
    
    * Simplify component configuration
    
    Co-authored-by: RikkaW <rikka@shizuku.moe>
    
    ---------
    
    Co-authored-by: RikkaW <rikka@shizuku.moe>

--- gradle-plugin/src/main/java/dev/rikka/tools/refine/RefinePlugin.java
+++ gradle-plugin/src/main/java/dev/rikka/tools/refine/RefinePlugin.java
@@ -2,12 +2,15 @@ package dev.rikka.tools.refine;
 
 import com.android.build.api.instrumentation.InstrumentationScope;
 import com.android.build.api.variant.AndroidComponentsExtension;
+import com.android.build.api.variant.Component;
+import com.android.build.api.variant.HasAndroidTest;
 import kotlin.Unit;
 import org.gradle.api.GradleException;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 
 /**
  * Gradle plugin that do the class rename works.
@@ -22,11 +25,21 @@ public class RefinePlugin implements Plugin<Project> {
 
         final AndroidComponentsExtension<?, ?, ?> components = target.getExtensions().getByType(AndroidComponentsExtension.class);
         components.onVariants(components.selector().all(), variant -> {
-            variant.getInstrumentation().transformClassesWith(
+            configureComponent(variant);
+            configureComponent(variant.getUnitTest());
+            if (variant instanceof HasAndroidTest) {
+                configureComponent(((HasAndroidTest) variant).getAndroidTest());
+            }
+        });
+    }
+
+    private void configureComponent(@Nullable final Component component) {
+        if (component != null) {
+            component.getInstrumentation().transformClassesWith(
                     RefineFactory.class,
                     InstrumentationScope.ALL,
                     (parameters) -> Unit.INSTANCE
             );
-        });
+        }
     }
 }
